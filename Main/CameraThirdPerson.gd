0bf6a24c719be7734fd9a0b225b9b9581add57f8
Jake Burga <jakerburga@gmail.com>
2022-10-19 23:24:40 -0400

    messing around with 3rd person camera

---
+++ Main/CameraThirdPerson.gd
@@ -0,0 +1,17 @@
+extends Camera
+
+export(NodePath) var look_at_target = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	var look_at_node = get_node(look_at_target) as Spatial
+	if look_at_node:
+		look_at(look_at_node.global_translation, Vector3.UP)
