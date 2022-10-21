98ca42cbc93ee25b3872dfb597f1d401c3ae7a32
Jake Burga <jakerburga@gmail.com>
2022-10-21 16:53:34 -0400

    messing around with navigation and navigation meshes

--- Character/Character3D.gd
+++ Character/Character3D.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 class_name Character3D
 
+export(NodePath) var target_position_3d = null
 export(float) var speed = 10
 export(bool) var jump_enabled = true
 export(float) var jump_speed = 10
@@ -8,16 +9,28 @@ export(float) var gravity = -10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if target_position_3d != null:
+		var target_position = get_node(target_position_3d) as Position3D
+		var location = target_position.global_translation
+		$NavigationAgent.set_target_location(location)
+		print($NavigationAgent.get_final_location())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _process(delta):
+	# example of using navigation to automatically move the character
+	if target_position_3d != null:
+		var target_position = get_node(target_position_3d) as Position3D
+		var location = target_position.global_translation
+		$NavigationAgent.set_target_location(location)
+
 var velocity = Vector3()
 var v_gravity = Vector3(0, gravity, 0)
 func _physics_process(delta):
+	
 	velocity += v_gravity * delta
 	
 	var v_move = Vector3()
@@ -35,6 +48,10 @@ func _physics_process(delta):
 	if jump_enabled and is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = jump_speed
+	
+	# how to set the velocity of the character when navigating
+	# var nav_direction = ($NavigationAgent.get_next_location() - global_translation).normalized()
+	# v_move = nav_direction * speed
 	
 	v_move = v_move.normalized() * speed
 	velocity.x = v_move.x
