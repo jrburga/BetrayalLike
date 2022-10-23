4a981201a8b1713aae1abdbb1968e5a44d4a15c6
Jake Burga <jakerburga@gmail.com>
2022-10-23 16:40:20 -0400

    messing around with highlights

--- Prototype/Prototype3D.gd
+++ Prototype/Prototype3D.gd
@@ -10,40 +10,51 @@ onready var grid_map = get_node("%GridMap") as GridMap
 onready var camera = get_node("%Camera") as Camera
 onready var character = get_node("%Character3D") as Character3D
 
+func find_grid_map_under_mouse():
+	var mouse_position = get_viewport().get_mouse_position()
+	var ray_length = 100
+	var cast_from = camera.project_ray_origin(mouse_position)
+	var cast_to = cast_from + camera.project_ray_normal(mouse_position) * ray_length
+
+	var space_state = get_world().direct_space_state
+	var result = space_state.intersect_ray(
+		cast_from, 
+		cast_to, 
+		[], # exclude
+		10000 # mask for grid map
+		)
+	
+	return result
+
 var mouse_button_down = false
 func _physics_process(delta):
 	if Engine.editor_hint:
 		return
-	var mouse = get_viewport().get_mouse_position()
-	
-	
-	
-	var ray_length = 100
-	var cast_from = camera.project_ray_origin(mouse)
-	var cast_to = cast_from + camera.project_ray_normal(mouse) * ray_length
 
 	var was_mouse_button_down = mouse_button_down
 	mouse_button_down = Input.is_mouse_button_pressed(1)
 	
 	var mouse_just_pressed = mouse_button_down and not was_mouse_button_down
+	var result = find_grid_map_under_mouse()
 	
-	if mouse_just_pressed:
-		var space_state = get_world().direct_space_state
-		var result = space_state.intersect_ray(
-			cast_from, 
-			cast_to, 
-			[], # exclude
-			10000
-			)
+	var highlight = get_node("%MIHighlight")
+	if result:
+		highlight.visible = true
+		var half_size = grid_map.cell_size / 2
+		var map_pos = grid_map.world_to_map(result.position + Vector3(0, 0.1, 0))
+		var h_pos = grid_map.map_to_world(map_pos.x, map_pos.y, map_pos.z)
+		highlight.translation = h_pos
+	else:
+		highlight.visible = false
 		
-		if result:
-			var half_size = grid_map.cell_size / 2
-			var map_pos = grid_map.world_to_map(result.position)
-			var center_pos = grid_map.map_to_world(map_pos.x, map_pos.y, map_pos.z) + half_size
-			center_pos.y = 0
-			
-			print(center_pos)
-			character.set_target_location(center_pos)
+	if mouse_just_pressed and result:
+		var half_size = grid_map.cell_size / 2
+		var map_pos = grid_map.world_to_map(result.position + Vector3(0, 0.1, 0))
+		var center_pos = grid_map.map_to_world(map_pos.x, map_pos.y, map_pos.z) + half_size
+		center_pos.y = 0
+		
+		print(center_pos)
+		character.set_target_location(center_pos)
 
 func _on_print_grid_map(value):
 	if !value:
